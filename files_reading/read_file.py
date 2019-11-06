166af15a394c3f0992da5095e91818e8fec2ca7d
ralph <ralph@Ralphs-MacBook-Pro-2.local>
2019-11-06 14:41:25 +0100

    Now with merging

--- files_reading/read_file.py
+++ files_reading/read_file.py
@@ -12,4 +12,28 @@ def tohdf5(filename):
     else:
         print('file type not yet supported')
 
-
+def merge_hdf5(filelist, combined_name):
+    i = 0
+    with h5py.File(combined_name +'.hdf5', 'w') as f:
+        typegrp = f.create_group('type')
+        metadatagrp = f.create_group('metadata')
+        datagrp = f.create_group('datasets')
+        procgrp = f.create_group('process')
+        
+        for filename in filelist:
+            if filename.split('.')[-1] != 'hdf5':
+                print('Warning: \''+filename+'\' is a non-hdf5 file. File has been ignored.')
+            elif filename.split('.')[0] == combined_name:
+                print('Warning: \''+filename+'\' already exists. File has been ignored and will be overwritten.')
+            else:
+                with h5py.File(filename, 'r') as source_f:
+                    source_f.copy('type/'+filename.split('.')[0], typegrp)
+                    source_f.copy('metadata/'+filename.split('.')[0], metadatagrp)
+                    source_f.copy('datasets/'+filename.split('.')[0], datagrp)
+                    try:
+                        source_f.copy('process/'+filename.split('.')[0], procgrp)
+                    except:
+                        pass
+                    i = i + 1
+                print('\''+filename+'\' successfully merged')
+    print(str(i)+'/'+str(len(filelist))+' files merged into \'' + combined_name +'.hdf5\'')
