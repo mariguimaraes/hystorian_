92508fcce5325c34534902be56f22e61c82c3f08
Loïc Musy <loic.musy@unige.ch>
2020-11-23 14:41:40 +0100

    TO TEST : compress_hdf5 now working
    The way m_apply store kwargs was changed to facilitate the implementation of compress_hdf5
    
    compress_hdf5 is a function that take a file and remove the processed data if they can be reconstructed, thus reducing files size.

--- multiscale/processing/core.py
+++ multiscale/processing/core.py
@@ -162,15 +162,20 @@ def m_apply(filename, function, in_paths, output_names=None, folder_names=None,
                     if prop_attrs is not None:
                         dataset = propagate_attrs(dataset, prop_attr_keys, prop_attr_vals)
                 write_generic_attributes(fproc[name], out_folder_location + '/', in_paths, name, function)
+                dict_kwargs = {}
                 for key, value in kwargs.items():
                     if value is None:
+                        dict_kwargs[key] = 'None'
                         value = 'None'
-                    try:
-                        dataset.attrs[key] = value
-                    except RuntimeError:
-                        print('Attribute was not able to be saved, probably because the attribute'
-                              'is too large')
-                        dataset.attrs[key] = 'None'
+                    else:
+                        dict_kwargs[key] = value
+                    #try:
+                    #    dataset.attrs[key] = value
+                    #except RuntimeError:
+                    #    print('Attribute was not able to be saved, probably because the attribute'
+                    #          'is too large')
+                    #    dataset.attrs[key] = 'None'
+                dataset.attrs['kwargs'] = str(dict_kwargs)
         else:
             print('Error: Unequal amount of outputs and output names')
     return result
--- multiscale/processing/experimental.py
+++ multiscale/processing/experimental.py
@@ -1,59 +1,56 @@
 import h5py
 import importlib
-from inspect import getmembers, isfunction
-from multiscale import io, processing
-import inspect
-import numpy as np
+from multiscale import processing
+import ast
 
-def my_import(name):
-    components = name.split('.')
-    print(components)
-    mod = __import__(components[0])
-    print(mod)
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-        print(mod)
-    return mod
 
 def compress_hdf5(file, error_threshold=0, bypass_verification=False):
+    identical = True
     if not bypass_verification:
         print('Please understand that this function WILL remove processed datas from your hdf5.\n '
               'Normally they should be rebuildable using existing functions. But no guarantee is given. \n '
               'Please write "I understand the risk"')
         input_value = input()
         if input_value != 'I understand the risk':
-            return -1
+            return None
 
-    #print([o for o in getmembers(multiscale.io) if isfunction(o[1])])
     with h5py.File(file, 'r+') as f:
         processes = f['process']
         processing_lst = processes.keys()
 
         for k in list(processing_lst)[::-1]:
-            module = importlib.import_module('.'.join(processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs['operation name'].split('.')[:-1]))
-            func = getattr(module, processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs['operation name'].split('.')[-1])
-            print(func.__name__)
-            result = func(f[processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs['source'][0]][()])
-            print(processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'][()])
-            print(f[processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs['source'][0]][()])
-            print(result)
-            if (result['data'] == processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'][()]).all():
-                print('OK')
+            fname = list(processes[k].keys())[0]
+            outputs = list(processes[k][fname])
+            print(outputs)
+            module = importlib.import_module('.'.join(
+                processes[k][fname][outputs[0]].attrs['operation name'].split('.')[:-1]))
 
-                tmpattrs = {}
-                for k2 in processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs.keys():
-                    (processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs[k2])
-                    tmpattrs[k2] = processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs[k2]
+            func = getattr(module,
+                           processes[k][fname][outputs[0]].attrs['operation name'].split('.')[-1])
+            kwargs = ast.literal_eval(processes[k][fname][outputs[0]].attrs['kwargs'])
+            inputs = []
+            for source in processes[k][fname][outputs[0]].attrs['source']:
+                inputs.append(f[source][()])
 
-                del(processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'])
-                processes[k]['SD_P4_zB5_050mV_-2550mV_0002'].create_dataset('Phase2Retrace', (1,), dtype=int)
-                for k3 in tmpattrs.keys():
-                    val = tmpattrs[k3]
-                    processes[k]['SD_P4_zB5_050mV_-2550mV_0002/Phase2Retrace'].attrs.__setitem__(k3,val)
-            else:
-                print('NOT OK')
+            results = func(*inputs, **kwargs)
+            for i in range(len(outputs)):
+                print(processes[k][fname][outputs[i]][()])
+                if (results[i] - processes[k][fname][outputs[i]][()] < error_threshold).all():
+                    print('Result is not identical, not compressing')
+                    identical = False
+                    break
+            if identical:
+                for i in range(len(outputs)):
+                    tmpattrs = {}
+                    for k2 in processes[k][fname][outputs[i]].attrs.keys():
+                        tmpattrs[k2] = processes[k][fname][outputs[i]].attrs[k2]
+
+                    del (processes[k][fname][outputs[i]])
+                    processes[k][fname].create_dataset(outputs[i], (1,), dtype=int)
+                    for k3 in tmpattrs.keys():
+                        val = tmpattrs[k3]
+                        processes[k][fname][outputs[i]].attrs.__setitem__(k3, val)
 
     processing.core.deallocate_hdf5_memory(file, verify=False)
-
 
 
