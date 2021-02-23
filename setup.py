d7d143ec0db0ae0eaf4523e84c4098f7cf84ef68
Loïc Musy <loic.musy@unige.ch>
2021-02-23 12:28:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import os
 
 here = pathlib.Path(__file__).parent.resolve()
 
-if os.environ.get('CI_COMMIT_TAG'):
-    version = os.environ['CI_COMMIT_TAG']
-else:
-    version = os.environ['CI_JOB_ID']
+#if os.environ.get('CI_COMMIT_TAG'):
+#    version = os.environ['CI_COMMIT_TAG']
+#else:
+#    version = os.environ['CI_JOB_ID']
 
 setuptools.setup(
     name="hystorian",
-    version=version,
+    version=0.0.1,
     author="Loïc Musy <loic.musy@unige.ch>, Ralph Bulanadi <ralph.bulanadi@unige.ch>",
     author_email="loic.musy@unige.ch",
     description="a generic materials science data analysis Python package built with processing traceability, reproducibility, and archival ability at its core.",
