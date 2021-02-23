951b2895118380685a433a7fdf451b2f696fe433
Loïc Musy <loic.musy@unige.ch>
2021-02-23 12:13:02 +0100

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import pathlib
+import os
+
+here = pathlib.Path(__file__).parent.resolve()
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
+
+setuptools.setup(
+    name="hystorian",
+    version=version,
+    author="Loïc Musy <loic.musy@unige.ch>, Ralph Bulanadi <ralph.bulanadi@unige.ch>",
+    author_email="loic.musy@unige.ch",
+    description="a generic materials science data analysis Python package built with processing traceability, reproducibility, and archival ability at its core.",
+    long_description=(here / 'README.md').read_text(encoding='utf-8'),
+    url='https://gitlab.unige.ch/paruch-group/hystorian',
+    packages=setuptools.find_packages(where='hystorian'),
+    license='CC-By 4.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC-By 4.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
